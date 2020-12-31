d63bf97b9acf6e5a9809c654219da3086b782b00
YuikoIto <toffywing@gmail.com>
2020-12-31 18:23:33 +0900

    add form helper

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
 <h1>{{ $user->name }}</h1>
+
+{!! Form::open(['url' => url('questions')]) !!}
+{!! Form::hidden('user', $user->unique_id) !!}
+{!! Form::textarea('body') !!}
+{!! Form::Submit('投稿する') !!}
+{!! Form::close() !!}
 @endsection
